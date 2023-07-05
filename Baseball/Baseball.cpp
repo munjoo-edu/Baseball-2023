ec7d54cf00b045f434905a9d09575b70b2dcae88
Munjoo Kim <munjoo.edu@gmail.com>
2023-07-06 08:29:54 +0900

    [F] Modi and clean parameter check logic
    Signed-off-by: Munjoo Kim <munjoo.edu@gmail.com>

--- Baseball/Baseball.cpp
+++ Baseball/Baseball.cpp
@@ -5,21 +5,31 @@ using namespace std;
 class Baseball
 {
 public:
-	void guess(const string& guessNumber) {
+	bool isDupNumber(const string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2];
+	}
+
+	void CheckParameter(const string& guessNumber)
+	{
 		if (guessNumber.length() != 3) {
 			throw length_error("Must be three letters.");
 		}
 
 		for (char ch : guessNumber) {
-			if (ch < '0' || ch >'9') {
-				throw invalid_argument("Must be number");
-			}
+			if (ch >= '0' && ch <= '9') continue;
+			throw invalid_argument("Must be number");
 		}
 
-		if (guessNumber[0] == guessNumber[1]
-			|| guessNumber[0] == guessNumber[2]
-			|| guessNumber[1] == guessNumber[2]) {
+		if (isDupNumber(guessNumber)) {
 			throw invalid_argument("Must not have the same number");
 		}
+	}
+
+	void guess(const string& guessNumber)
+	{
+		CheckParameter(guessNumber);
 	}
 };
